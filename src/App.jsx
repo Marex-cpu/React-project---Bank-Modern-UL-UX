cc52f8d5ebd6794ad9da468063abfecce0860e7e
marko-petrovic <markoo3871@gmail.com>
2022-12-07 14:04:11 +0100

    navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React from 'react';
+import styles from './styles';
+
+import { Navbar, Billing, Business, Button, CardDeal, CTA, Clients, FeedbackCard, Footer, GetStarted, Hero, Stats, Testimonials } from './components';
 
 const App = () => {
   return (
-    <div></div>
+    <div className='bg-primary w-full overflow-hidden'>
+
+      {/* nav-bar */}
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar/>
+        </div>
+      </div>
+
+      {/* hero-section */}
+      {/* <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Hero
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Stats
+          Business
+          Billing
+          CardDeal
+          Testimonials
+          Clients
+          CTA
+          Footer
+        </div>
+      </div> */}
+    </div>
   )
 }
 
