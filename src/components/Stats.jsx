ad39d7c807db635e81e547cfc92e591b1bd973c7
marko-petrovic <markoo3871@gmail.com>
2022-12-09 11:23:06 +0100

    stats component

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
+import styles from '../styles';
+import { stats } from '../constants/texts';
 
 const Stats = () => {
   return (
-    <div>Stats</div>
+    <section className={`${styles.flexCenter} ss:flex-row flex-col sm:mb-20 mb-6`}>
+      {stats.map((stat) => {
+        return (
+          <div key={stat.id} className={`flex-1 flex justify-center items-center flex-row m-2`}>
+            <h4 className='font-poppins font-semibold md:text-[40px] sm:text-[25px] text-[18px] md:leading-[53px] leading-[43px] text-white'>{stat.value}</h4>
+            <p className='font-poppins font-noraml md:text-[20px] sm:text-[15px] text-[12px] md:leading-[26px] leading-[21px] text-gradient uppercase ml-3'>{stat.title}</p>
+          </div>
+        )
+      })}
+    </section>
   )
 }
 
