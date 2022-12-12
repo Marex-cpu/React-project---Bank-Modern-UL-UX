7e9bf7c75b8324e425f7dc8c2c7f6c43613ff12c
marko-petrovic <markoo3871@gmail.com>
2022-12-12 14:51:22 +0100

    add footer component

--- src/App.jsx
+++ src/App.jsx
@@ -28,6 +28,9 @@ const App = () => {
           <Billing/>
           <CardDeal/>
           <Testimonials/>
+          <Clients/>
+          <CTA/>
+          <Footer/>
         </div>
       </div> 
     </div>
