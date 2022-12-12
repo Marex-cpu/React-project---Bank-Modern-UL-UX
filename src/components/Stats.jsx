e462d6146fffa73cf968b0b11cc263c053accdcf
marko-petrovic <markoo3871@gmail.com>
2022-12-12 10:24:08 +0100

    business component

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -4,12 +4,12 @@ import { stats } from '../constants/texts';
 
 const Stats = () => {
   return (
-    <section className={`${styles.flexCenter} ss:flex-row flex-col sm:mb-20 mb-6`}>
+    <section className={`${styles.flexCenter} flex-row flex-wrap md:mb-20 mb-0`}>
       {stats.map((stat) => {
         return (
-          <div key={stat.id} className={`flex-1 flex justify-center items-center flex-row m-2`}>
-            <h4 className='font-poppins font-semibold md:text-[40px] sm:text-[25px] text-[18px] md:leading-[53px] leading-[43px] text-white'>{stat.value}</h4>
-            <p className='font-poppins font-noraml md:text-[20px] sm:text-[15px] text-[12px] md:leading-[26px] leading-[21px] text-gradient uppercase ml-3'>{stat.title}</p>
+          <div key={stat.id} className={`flex flex-1 justify-center items-center flex-row  m-2`}>
+            <h4 className='font-poppins font-semibold md:text-[40px] sm:text-[30px] text-[25px] md:leading-[53px] leading-[43px] text-white'>{stat.value}</h4>
+            <p className='font-poppins font-noraml md:text-[20px] sm:text-[16px] text-[14px] md:leading-[26px] leading-[21px] text-gradient uppercase ml-3'>{stat.title}</p>
           </div>
         )
       })}
